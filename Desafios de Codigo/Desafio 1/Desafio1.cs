3eb60393477cfa99246190503332c84b6059d3f3
AndreVeloso <40570417+AndreVeloso@users.noreply.github.com>
2022-05-30 19:08:41 -0300

    Add files via upload

---
+++ Desafios de Codigo/Desafio 1/Desafio1.cs
@@ -0,0 +1,21 @@
+//Abaixo segue um exemplo de código que você pode ou não utilizar
+//using System;
+
+//namespace Start
+//{
+//   class Program
+// {
+//   static void Main(string[] args)
+// {
+//   double A, B, C;
+// A = double.Parse(Console.ReadLine());
+//B = double.Parse(Console.ReadLine());
+// C = double.Parse(Console.ReadLine());
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+//double MEDIA = ((A * 2.0) + (B * 3.0) + (C * 5.0)) / (10.0);
+
+//Console.WriteLine($"MEDIA = {MEDIA.ToString("N1")}");
+//Console.ReadKey();
+//}
+//}
+//}
